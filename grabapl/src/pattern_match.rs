4accfd3efc46380ea3c98637d688a420f9ef94c6
Niels Saurer <me@nielssaurer.com>
2025-04-27 23:55:24 +0200

    notes

--- grabapl/src/pattern_match.rs
+++ grabapl/src/pattern_match.rs
@@ -27,7 +27,12 @@ enum AbsoluteMatchOrder {
 }
 
 enum RelativeMatchOrder {
-    ExactOffset(NonZeroI32), // TODO: define variants with and without wraparound?
+    // TODO: define variants with and without wraparound?
+    ExactOffset(NonZeroI32),
+    // Probably *dont* want these to be wraparound, since otherwise they're useless.
+    // a wraparound "before" constraint on two edges is meaningless.
+    // It would only become meaningful if you had more than two edges and defined a total order
+    // over all three with the constraint that it must be <= one full loop.
     Before,
     After,
 }
@@ -45,7 +50,25 @@ Inner operation: input A' with "FromStart(0)" edge to B'
 Should we now use the abstract graph from outer operation? In which case, FromStart(0) will match A->B?
 Or should we go back all the way to the concrete graph, in which case we would get the X->Y edge?
 
-I probably think the former, even though that may be confusing? 
+I probably think the former, even though that may be confusing?
+
+how are edges ordered for a given visual representation? I would say according to their visual position.
+
+Can the user override a given edge? eg by also passing the node it should match to? seems complicated.
+
+
+
+What if we wanted to represent current Algot Web semantics using this system?
+Going 'outwards' from the input node, every edge gets an Absolute(FromStart(..)) order. Once we pivot, we get a "Relative(ExactOffset(+/-1))" order.
+
+
+TODO Big question:
+How to handle incoming vs. outgoing edges? Independently? i.e., for dir in [Incoming, Outgoing], we have a separate
+order incl separate LocalEdgeId etc?
+Think about if that can cause issues!
+
+
+
 
 */
 
