8e8a2c998e8fbf042328ae2e0a33de78fa7607ba
Niels Saurer <me@nielssaurer.com>
2025-05-17 18:09:22 +0200

    dont ignore wbg

---
+++ example_clients/simple_semantics/simple-semantics-js/wbg/simple_semantics_ffi_bg.wasm.d.ts
@@ -0,0 +1,25 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const AbstractGraph_destroy: (a: number) => void;
+export const ConcreteGraph_create: () => number;
+export const ConcreteGraph_add_node: (a: number, b: number) => void;
+export const ConcreteGraph_say_hi: (a: number) => void;
+export const ConcreteGraph_destroy: (a: number) => void;
+export const DotCollector_create: () => number;
+export const DotCollector_collect: (a: number, b: number) => void;
+export const DotCollector_get_dot: (a: number, b: number) => void;
+export const DotCollector_destroy: (a: number) => void;
+export const diplomat_init: () => void;
+export const diplomat_simple_write: (a: number, b: number, c: number) => void;
+export const diplomat_buffer_write_create: (a: number) => number;
+export const diplomat_buffer_write_get_bytes: (a: number) => number;
+export const diplomat_buffer_write_len: (a: number) => number;
+export const diplomat_buffer_write_destroy: (a: number) => void;
+export const diplomat_alloc: (a: number, b: number) => number;
+export const diplomat_free: (a: number, b: number, c: number) => void;
+export const diplomat_is_str: (a: number, b: number) => number;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_export_2: WebAssembly.Table;
+export const __wbindgen_start: () => void;
