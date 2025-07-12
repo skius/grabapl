803ff9f2991c8ce794418cdc18c7ffcbd9a442f2
Niels Saurer <me@nielssaurer.com>
2025-07-12 23:19:24 +0200

    more comment cleanup

--- grabapl/examples/notes.rs
+++ grabapl/examples/notes.rs
@@ -42,7 +42,9 @@ TODO: Add option to ignore parent order?
 // Because we'll want to work abstractly with a pattern graph, we'll want the pattern type to be the type that pattern matches against.
 // In other words, we want the pattern type to be the analogue of the PL-"type", with subtyping. eg. a wildcard is just the analogue of the Top type
 
-use grabapl::SubstMarker;
+use grabapl::{Semantics, SubstMarker};
+use grabapl::operation::query::{ShapeNodeIdentifier};
+use grabapl::operation::signature::parameter::OperationParameter;
 
 pub struct WithSubstMarker<T> {
     marker: SubstMarker,
@@ -166,5 +168,122 @@ impl<T> WithSubstMarker<T> {
 //         Ok(())
 //     }
 // }
+
+
+pub struct AbstractQueryOutput<S: Semantics> {
+    pub changes: Vec<AbstractQueryChange<S>>,
+}
+
+pub enum AbstractQueryChange<S: Semantics> {
+    ExpectNode(NodeChange<S>),
+    ExpectEdge(EdgeChange<S>),
+}
+
+pub enum NodeChange<S: Semantics> {
+    NewNode(SubstMarker, S::NodeAbstract),
+}
+
+pub enum EdgeChange<S: Semantics> {
+    // TODO: maybe use AbstractNodeId as input for the SubstMarkers?
+    ChangeEdgeValue {
+        from: SubstMarker,
+        to: SubstMarker,
+        edge: S::EdgeAbstract,
+    },
+}
+
+pub struct ShapeQuery<S: Semantics> {
+    // The context abstract graph to expect
+    pub parameter: OperationParameter<S>,
+    pub changes: Vec<ShapeQueryChange<S>>,
+}
+
+
+#[derive(Copy, Clone)]
+pub enum AbstractShapeNodeIdentifier {
+    /// A node in the parameter graph.
+    ParameterMarker(SubstMarker),
+    /// A node that is expected from this shape query.
+    ShapeQueryNode(ShapeNodeIdentifier),
+}
+
+pub enum ShapeQueryChange<S: Semantics> {
+    ExpectNode(ShapeNodeChange<S>),
+    ExpectEdge(ShapeEdgeChange<S>),
+}
+
+pub enum ShapeNodeChange<S: Semantics> {
+    /// Expect a new node with the given abstract value and give it the identifier
+    NewNode(ShapeNodeIdentifier, S::NodeAbstract),
+}
+
+pub enum ShapeEdgeChange<S: Semantics> {
+    /// Expect an edge from the node with the given identifier to the node with the given identifier, with the given abstract value
+    ExpectedEdgeValue {
+        from: AbstractShapeNodeIdentifier,
+        to: AbstractShapeNodeIdentifier,
+        /// The expected abstract edge value
+        edge: S::EdgeAbstract,
+    },
+}
+
+// pub enum GraphShapeQueryNodeWrapper<S: Semantics> {
+//     /// A node that is expected from this shape query.
+//     ShapeQueryNode(ShapeNodeIdentifier, S::NodeAbstract),
+//     /// A node that has already existed.
+//     ExistingNode(NodeKey),
+// }
+//
+// pub enum GraphShapeQueryEdgeWrapper<S: Semantics> {
+//     /// An edge that is expected from this shape query.
+//     ExpectedEdgeValue {
+//         from: AbstractShapeNodeIdentifier,
+//         to: AbstractShapeNodeIdentifier,
+//         edge: S::EdgeAbstract,
+//     },
+//     /// An edge that has already existed.
+//     ExistingEdge(NodeKey, NodeKey),
+// }
+//
+// pub struct GraphShapeQuery<S: Semantics> {
+//     pub parameter: OperationParameter<S>,
+//     pub expected_graph: Graph<GraphShapeQueryNodeWrapper<S>, GraphShapeQueryEdgeWrapper<S>>,
+// }
+
+
+// TODO: What to do about operations that conditionally _remove_ nodes or edges?
+//  This implies that our abstract graph may not only be an underapproximation of the concrete graph, but also an overapproximation.
+//  This is a problem because we expect anything we see in the abstract graph can be used concretely.
+//  One fix might be to turn the abstract change of a "conditional remove" to just abstractly always remove. Then the user would have
+//  to check again if something is present, so the same behavior as if we instead added something. This is tedious, but should work.
+
+// TODO: wrt above, the same goes for operations that conditionally _change_ an abstract value. I think it should be the "merge"
+//  of the new value and the old value, where the old value is the actual _argument_ abstract value, not the _parameter_ (potentially upcast) abstract
+//  value that is defined in the child operation. So basically, the operation says "ChangeTo(new abstract value)", and then the caller has to
+//  update its abstract graph accordingly with the merge.
+
+// TODO: Note:
+//  What if shape queries had just one builtin, and that builtin was of the form:
+//  1. This is my current abstract graph
+//  2. Let me make 'pseudo' changes to it. For example, I add a node, and set it as the child of some existing node.
+//  3. The query tells me if this matches.
+//  How would that work?
+//  As the writer of a user defined op, I would need to have know my current abstract graph. We kind of do have that atm I guess? it's the parameter + the sequence of all instructions
+//  Then I propose changes. Like NewNode(some ident), AbstractValue(some ident, like new node, param, or dynamic output), Edge, etc.
+//  Then I can call the query with those two args (abstract graph, proposed changes) and act based on true/false.
+//  Okay.
+//  How does the query work?
+//  Abstractly it's clear what changes. So do we even need that?
+//  Concretely, it's more difficult I think:
+//  1. We have the concrete graph
+//  2. We have the input abstract graph.
+//   - Here we should have some known mapping from concrete to abstract (Side note: potentially again a problem with assigning one node to multiple abstract nodes)
+//  3. We also have the proposed changes
+//   - With these changes we can build a 'new' abstract graph
+//  Can we now use isomorphisms to find a mapping from the new abstract graph (the subgraph) to the concrete graph, that:
+//   a) makes sure unchanged nodes in the abstract graph still get mapped to the same nodes in the concrete graph
+//   b) changed nodes in the abstract graph can be matched against the ToAbstract version of the concrete nodes' values
+//
+//   Can the input abstract graph just be a subgraph of the actual abstract graph? Just enough to imply all the necessary context like "next child" or "prev child" or similar?
 
 fn main() {}
--- grabapl/src/operation/query.rs
+++ grabapl/src/operation/query.rs
@@ -13,149 +13,29 @@ use petgraph::algo::general_subgraph_monomorphisms_iter;
 use petgraph::visit::NodeIndexable;
 use std::collections::{HashMap, HashSet};
 
-pub struct AbstractQueryOutput<S: Semantics> {
-    pub changes: Vec<AbstractQueryChange<S>>,
-}
-
-pub struct ConcreteQueryOutput {
-    pub taken: bool,
-}
-
-pub enum AbstractQueryChange<S: Semantics> {
-    ExpectNode(NodeChange<S>),
-    ExpectEdge(EdgeChange<S>),
-}
-
-pub enum NodeChange<S: Semantics> {
-    NewNode(SubstMarker, S::NodeAbstract),
-}
-
-pub enum EdgeChange<S: Semantics> {
-    // TODO: maybe use AbstractNodeId as input for the SubstMarkers?
-    ChangeEdgeValue {
-        from: SubstMarker,
-        to: SubstMarker,
-        edge: S::EdgeAbstract,
-    },
-}
-
-// TODO: What to do about operations that conditionally _remove_ nodes or edges?
-//  This implies that our abstract graph may not only be an underapproximation of the concrete graph, but also an overapproximation.
-//  This is a problem because we expect anything we see in the abstract graph can be used concretely.
-//  One fix might be to turn the abstract change of a "conditional remove" to just abstractly always remove. Then the user would have
-//  to check again if something is present, so the same behavior as if we instead added something. This is tedious, but should work.
-
-// TODO: wrt above, the same goes for operations that conditionally _change_ an abstract value. I think it should be the "merge"
-//  of the new value and the old value, where the old value is the actual _argument_ abstract value, not the _parameter_ (potentially upcast) abstract
-//  value that is defined in the child operation. So basically, the operation says "ChangeTo(new abstract value)", and then the caller has to
-//  update its abstract graph accordingly with the merge.
-
-// TODO: Note:
-//  What if shape queries had just one builtin, and that builtin was of the form:
-//  1. This is my current abstract graph
-//  2. Let me make 'pseudo' changes to it. For example, I add a node, and set it as the child of some existing node.
-//  3. The query tells me if this matches.
-//  How would that work?
-//  As the writer of a user defined op, I would need to have know my current abstract graph. We kind of do have that atm I guess? it's the parameter + the sequence of all instructions
-//  Then I propose changes. Like NewNode(some ident), AbstractValue(some ident, like new node, param, or dynamic output), Edge, etc.
-//  Then I can call the query with those two args (abstract graph, proposed changes) and act based on true/false.
-//  Okay.
-//  How does the query work?
-//  Abstractly it's clear what changes. So do we even need that?
-//  Concretely, it's more difficult I think:
-//  1. We have the concrete graph
-//  2. We have the input abstract graph.
-//   - Here we should have some known mapping from concrete to abstract (Side note: potentially again a problem with assigning one node to multiple abstract nodes)
-//  3. We also have the proposed changes
-//   - With these changes we can build a 'new' abstract graph
-//  Can we now use isomorphisms to find a mapping from the new abstract graph (the subgraph) to the concrete graph, that:
-//   a) makes sure unchanged nodes in the abstract graph still get mapped to the same nodes in the concrete graph
-//   b) changed nodes in the abstract graph can be matched against the ToAbstract version of the concrete nodes' values
-//
-//   Can the input abstract graph just be a subgraph of the actual abstract graph? Just enough to imply all the necessary context like "next child" or "prev child" or similar?
 pub trait BuiltinQuery {
     type S: Semantics;
 
     /// The pattern to match against the graph.
     fn parameter(&self) -> OperationParameter<Self::S>;
 
-    // TODO: methods to describe abstract shape changes, and a method to dynamically determine which path to take
-    //  perhaps the abstract shape change should just more or less be a const fn that returns (Taken, NotTaken) changes?
-    //  And concrete just returns which path to take?
-
-    // TODO ^ above is obsolete I think
-
     // TODO: add invariant (checked?) that the abstract graph does not get new nodes or deleted nodes.
     //  actually, do we really need modification at all? ...
 
     fn apply_abstract(&self, g: &mut GraphWithSubstitution<AbstractGraph<Self::S>>);
-    // ) -> AbstractQueryOutput<Self::S>;
 
     // TODO: if we decide to actually support modification, we need to include an OperationOutput so that we can support new nodes and can keep track of
     //  changes of av's.
     fn query(&self, g: &mut GraphWithSubstitution<ConcreteGraph<Self::S>>) -> ConcreteQueryOutput;
 }
 
-pub struct ShapeQuery<S: Semantics> {
-    // The context abstract graph to expect
-    pub parameter: OperationParameter<S>,
-    pub changes: Vec<ShapeQueryChange<S>>,
+pub struct ConcreteQueryOutput {
+    pub taken: bool,
 }
 
 #[derive(Debug, Clone, Copy, Hash, Eq, PartialEq, From, Into)]
 pub struct ShapeNodeIdentifier(Intern<String>);
 interned_string_newtype!(ShapeNodeIdentifier);
-
-#[derive(Copy, Clone)]
-pub enum AbstractShapeNodeIdentifier {
-    /// A node in the parameter graph.
-    ParameterMarker(SubstMarker),
-    /// A node that is expected from this shape query.
-    ShapeQueryNode(ShapeNodeIdentifier),
-}
-
-pub enum ShapeQueryChange<S: Semantics> {
-    ExpectNode(ShapeNodeChange<S>),
-    ExpectEdge(ShapeEdgeChange<S>),
-}
-
-pub enum ShapeNodeChange<S: Semantics> {
-    /// Expect a new node with the given abstract value and give it the identifier
-    NewNode(ShapeNodeIdentifier, S::NodeAbstract),
-}
-
-pub enum ShapeEdgeChange<S: Semantics> {
-    /// Expect an edge from the node with the given identifier to the node with the given identifier, with the given abstract value
-    ExpectedEdgeValue {
-        from: AbstractShapeNodeIdentifier,
-        to: AbstractShapeNodeIdentifier,
-        /// The expected abstract edge value
-        edge: S::EdgeAbstract,
-    },
-}
-
-// pub enum GraphShapeQueryNodeWrapper<S: Semantics> {
-//     /// A node that is expected from this shape query.
-//     ShapeQueryNode(ShapeNodeIdentifier, S::NodeAbstract),
-//     /// A node that has already existed.
-//     ExistingNode(NodeKey),
-// }
-//
-// pub enum GraphShapeQueryEdgeWrapper<S: Semantics> {
-//     /// An edge that is expected from this shape query.
-//     ExpectedEdgeValue {
-//         from: AbstractShapeNodeIdentifier,
-//         to: AbstractShapeNodeIdentifier,
-//         edge: S::EdgeAbstract,
-//     },
-//     /// An edge that has already existed.
-//     ExistingEdge(NodeKey, NodeKey),
-// }
-//
-// pub struct GraphShapeQuery<S: Semantics> {
-//     pub parameter: OperationParameter<S>,
-//     pub expected_graph: Graph<GraphShapeQueryNodeWrapper<S>, GraphShapeQueryEdgeWrapper<S>>,
-// }
 
 pub struct GraphShapeQuery<S: Semantics> {
     // TODO: perhaps we don't need a fullblown OperationParameter here, since we don't really need SubstMarker?
@@ -172,7 +52,7 @@ pub struct GraphShapeQuery<S: Semantics> {
 }
 
 pub struct ConcreteShapeQueryResult {
-    // the node_keys here are the concrete keys of the mapped graph
+    /// The `NodeKey`s are the concrete keys of the real graph
     /// Some(mapping) if the shape query matched, None if it did not match.
     pub shape_idents_to_node_keys: Option<HashMap<ShapeNodeIdentifier, NodeKey>>,
 }
@@ -182,9 +62,6 @@ pub(crate) fn run_builtin_query<S: Semantics>(
     query: &S::BuiltinQuery,
     arg: OperationArgument,
 ) -> OperationResult<ConcreteQueryOutput> {
-    // let param = query.parameter();
-    // let abstract_graph = S::concrete_to_abstract(g);
-    // let subst = get_substitution(&abstract_graph, &param, &selected_inputs)?;
     let mut gws = GraphWithSubstitution::new(g, &arg.subst);
     let output = query.query(&mut gws);
     Ok(output)
