36a43cfae775e00a963371064ba70e75b737c175
Niels Saurer <me@nielssaurer.com>
2025-07-16 22:54:37 +0200

    add sample syntax to max heap example

--- grabapl/tests/algot_examples_tasks/task2.rs
+++ grabapl/tests/algot_examples_tasks/task2.rs
@@ -226,6 +226,8 @@ fn proptest_max_heap_remove_heap() {
     let mut op_ctx = OperationContext::<TestSemantics>::new();
     populate_max_heap_remove_op(&mut op_ctx);
 
+    eprintln!("serialized_op_ctx:\n{}", serde_json::to_string_pretty(&op_ctx).unwrap());
+
     proptest!(
         Config::with_cases(10),
         |(values in proptest::collection::vec(0..5000, 0..=10))| {
@@ -264,3 +266,65 @@ fn proptest_max_heap_remove_heap() {
         }
     );
 }
+
+
+/*
+Playing around with some invented syntax for the two operations:
+
+
+def max_heap_remove(sentinel: Object) -> (max_value: Integer) {
+    // ! syntax: take the single return value of the operation and bind to it.
+    // alternative is let map = add_node(...); and then map.new is the returned node.
+    let! max_value = add_node(-1);
+    if shape [
+        root: Integer,
+        sentinel -> root: Wildcard
+    ] {
+        // if we have a root, we can proceed
+        max_heap_remove_helper(root, max_value);
+    } else {
+        // do nothing
+    }
+    return (max_value: max_value);
+}
+
+
+def max_heap_remove_helper(root: Integer, max_value: Integer) {
+    // return the value of the root node
+    copy_value_from_to(root, max_value);
+    if shape [
+        left: Integer,
+        root -> left: Wildcard,
+        right: Integer,
+        root -> right: Wildcard
+    ] {
+        // we have two children, check which is larger
+        // method[] syntax: pass arguments to builtin operations
+        if cmp_fst_snd[>](left, right) {
+            // left is larger, recurse on left
+            let! temp_max = add_node(-1);
+            max_heap_remove_helper(left, temp_max);
+            copy_value_from_to(temp_max, root);
+            remove_node(temp_max);
+        } else {
+            // right is larger or equal, recurse on right
+            let! temp_max = add_node(-1);
+            max_heap_remove_helper(right, temp_max);
+            copy_value_from_to(temp_max, root);
+            remove_node(temp_max);
+        }
+    } else if shape [
+        child: Integer,
+        root -> child: Wildcard
+    ] {
+        // we have one child, recurse on it
+        let! temp_max = add_node(int(-1));
+        max_heap_remove_helper(child, temp_max);
+        copy_value_from_to(temp_max, root);
+        remove_node(temp_max);
+    } else {
+        // no children, we can delete the root node
+        remove_node(root);
+    }
+}
+*/
