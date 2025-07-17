f2fe0cfb94519e597a7eb8e55e3345bfa6ca6aa3
Niels Saurer <me@nielssaurer.com>
2025-07-17 17:27:15 +0200

    work on syntax parsing

--- syntax/src/lib.rs
+++ syntax/src/lib.rs
@@ -1,68 +1,178 @@
-/*
-This crate should eventually support parsing functions such as:
+pub mod minirust;
 
-def max_heap_remove(sentinel: Object) -> (max_value: Integer) {
-    // ! syntax: take the single return value of the operation and bind to it.
-    // alternative is let map = add_node(...); and then map.new is the returned node.
-    let! max_value = add_node(-1);
-    if shape [
-        root: Integer,
-        sentinel -> root: Wildcard
-    ] {
-        // if we have a root, we can proceed
-        max_heap_remove_helper(root, max_value);
-    } else {
-        // do nothing
-    }
-    return (max_value: max_value);
+use std::fmt;
+use std::fmt::Debug;
+use chumsky::{input::ValueInput, prelude::*};
+
+pub trait CustomSyntax: Clone + Debug {
+    type ArgType: Clone + fmt::Debug + Default;
+
+    fn get_arg_parser<'src>() -> impl Parser<'src, &'src str, Self::ArgType, extra::Err<Rich<'src, char, Span>>>;
 }
 
+#[derive(Clone, Debug, PartialEq)]
+pub struct MyCustomSyntax;
 
-def max_heap_remove_helper(root: Integer, max_value: Integer) {
-    // return the value of the root node
-    copy_value_from_to(root, max_value);
-    if shape [
-        left: Integer,
-        root -> left: Wildcard,
-        right: Integer,
-        root -> right: Wildcard
-    ] {
-        // we have two children, check which is larger
-        // method[] syntax: pass arguments to builtin operations
-        if cmp_fst_snd[>](left, right) {
-            // left is larger, recurse on left
-            let! temp_max = add_node(-1);
-            max_heap_remove_helper(left, temp_max);
-            copy_value_from_to(temp_max, root);
-            remove_node(temp_max);
-        } else {
-            // right is larger or equal, recurse on right
-            let! temp_max = add_node(-1);
-            max_heap_remove_helper(right, temp_max);
-            copy_value_from_to(temp_max, root);
-            remove_node(temp_max);
+impl CustomSyntax for MyCustomSyntax {
+    type ArgType = Vec<String>;
+
+    fn get_arg_parser<'src>() -> impl Parser<'src, &'src str, Self::ArgType, extra::Err<Rich<'src, char, Span>>> {
+        // consume [, then a comma separated list of strings, then ]
+        just('[')
+            .ignore_then(
+                any::<&'src str, extra::Err<Rich<'src, char, Span>>>()
+                    .filter(|c| *c != ']' && *c != ',')
+                    .repeated()
+                    .to_slice()
+                    .separated_by(just(','))
+                    .collect(),
+            )
+            .then_ignore(just(']'))
+            .map(|args: Vec<&'src str>| args.into_iter().map(String::from).collect())
+            .padded()
+    }
+}
+
+// A few type definitions to be used by our parsers below
+pub type Span = SimpleSpan;
+pub type Spanned<T> = (T, crate::minirust::Span);
+
+#[derive(Clone, Debug, PartialEq)]
+pub enum Token<'src> {
+    // do we want these?
+    Bool(bool),
+    Num(f64),
+    // Str(&'src str),
+    // Op(&'src str),
+    Arrow,
+    Ctrl(char),
+    Ident(&'src str),
+    Fn,
+    Let,
+    LetBang,
+    If,
+    Else,
+    Shape,
+    ClientProvided(&'src str),
+}
+
+impl fmt::Display for Token<'_> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Token::Bool(b) => write!(f, "{}", b),
+            Token::Num(n) => write!(f, "{}", n),
+            // Token::Str(s) => write!(f, "\"{}\"", s),
+            // Token::Op(op) => write!(f, "{}", op),
+            Token::Arrow => write!(f, "->"),
+            Token::Ctrl(c) => write!(f, "{}", c),
+            Token::Ident(i) => write!(f, "{}", i),
+            Token::Fn => write!(f, "fn"),
+            Token::Let => write!(f, "let"),
+            Token::LetBang => write!(f, "let!"),
+            Token::If => write!(f, "if"),
+            Token::Else => write!(f, "else"),
+            Token::Shape => write!(f, "shape"),
+            Token::ClientProvided(s) => write!(f, "{}", s),
         }
-    } else if shape [
-        child: Integer,
-        root -> child: Wildcard
-    ] {
-        // we have one child, recurse on it
-        let! temp_max = add_node(int(-1));
-        max_heap_remove_helper(child, temp_max);
-        copy_value_from_to(temp_max, root);
-        remove_node(temp_max);
-    } else {
-        // no children, we can delete the root node
-        remove_node(root);
     }
 }
 
 
-// some more syntax:
+pub fn lexer<'src>(
+) -> impl Parser<'src, &'src str, Vec<Spanned<Token<'src>>>, extra::Err<Rich<'src, char, Span>>> {
+    // A parser for numbers
+    let num = text::int(10)
+        .then(just('.').then(text::digits(10)).or_not())
+        .to_slice()
+        .from_str()
+        .unwrapped()
+        .map(Token::Num);
 
-// ?ident marks a node as a context node that will implicitly be matched.
-def takes_context_nodes(child: Integer, ?parent: Object, child -> parent: "child") {
 
+    // A parser for control characters (delimiters, semicolons, etc.)
+    let ctrl = one_of("()[]{};,?").map(Token::Ctrl);
+
+    let arrow = just("->").to(Token::Arrow);
+
+    // A parser for identifiers and keywords
+    let ident = text::ascii::ident().map(|ident: &str| match ident {
+        "fn" => Token::Fn,
+        "let!" => Token::LetBang,
+        "let" => Token::Let,
+        "if" => Token::If,
+        "else" => Token::Else,
+        "shape" => Token::Shape,
+        "true" => Token::Bool(true),
+        "false" => Token::Bool(false),
+        _ => Token::Ident(ident),
+    });
+
+    // '[', any text except '[', ']', then ']'. eg: [arg1, arg2, arg3]
+    let client_provided_arg = any::<&'src str, extra::Err<Rich<'src, char, Span>>>()
+        .filter(|c| *c != '[' && *c != ']')
+        .repeated()
+        .to_slice()
+        .map(Token::ClientProvided);
+
+    // A single token can be one of the above
+    let token = num.or(client_provided_arg).or(arrow).or(ctrl).or(ident);
+
+    let comment = just("//")
+        .then(any().and_is(just('\n').not()).repeated())
+        .padded();
+
+    token
+        .map_with(|tok, e| (tok, e.span()))
+        .padded_by(comment.repeated())
+        .padded()
+        // If we encounter an error, skip and attempt to lex the next character as a token instead
+        .recover_with(skip_then_retry_until(any().ignored(), end()))
+        .repeated()
+        .collect()
 }
 
-*/
+#[derive(Debug)]
+pub enum Expr<'src, CS: CustomSyntax> {
+    FnCall {
+        name: Spanned<&'src str>,
+        args: Spanned<CS::ArgType>,
+    }
+}
+
+
+
+pub fn first_parser<'tokens, 'src: 'tokens, I, CS: CustomSyntax>(
+) -> impl Parser<'tokens, I, Spanned<Expr<'src, CS>>, extra::Err<Rich<'tokens, Token<'src>, Span>>> + Clone
+where
+    I: ValueInput<'tokens, Token = Token<'src>, Span = Span>,
+{
+    let ident = select! {
+        Token::Ident(ident) => ident,
+    }.labelled("identifier");
+
+    let client_provided = select! {
+        Token::ClientProvided(arg) => arg,
+    }.labelled("client provided argument");
+
+    // A parser for function calls
+    let fn_call = ident
+        .then(client_provided)
+        // .map_with(|(name, args), e| Expr::FnCall {
+        .try_map_with(|(name, args_src), e| {
+            // parse args_src with CS::get_arg_parser()
+            let args = CS::get_arg_parser()
+                .parse(args_src)
+                .into_result().map_err(|_| {
+                    Rich::custom(e.span(), format!("Failed to parse arguments: {}", args_src))
+            })?;
+            Ok(Expr::FnCall {
+                name: (name, e.span()),
+                args: (args, e.span()),
+            })
+        });
+
+    // The main parser that returns the expression
+    fn_call
+        .map_with(|expr, e| (expr, e.span()))
+}
+
--- syntax/src/main.rs
+++ syntax/src/main.rs
@@ -1,3 +1,135 @@
+//! This is an entire parser and interpreter for a dynamically-typed Rust-like expression-oriented
+//! programming language. See `sample.nrs` for sample source code.
+//! Run it with the following command:
+//! cargo run --features="label" --example nano_rust -- examples/sample.nrs
+
+use ariadne::{sources, Color, Label, Report, ReportKind};
+use chumsky::{input::ValueInput, prelude::*};
+use std::{collections::HashMap, env, fmt, fs};
+
+use syntax::*;
+
 fn main() {
-    println!("Hello, world!");
+    let filename = env::args().nth(1).expect("Expected file argument");
+    let src = fs::read_to_string(&filename).expect("Failed to read file");
+
+    let (tokens, mut errs) = lexer().parse(src.as_str()).into_output_errors();
+
+    println!("Tokens: {tokens:?}");
+
+    let parse_errs = if let Some(tokens) = &tokens {
+        let (ast, parse_errs) = first_parser::<_, MyCustomSyntax>()
+            .map_with(|ast, e| (ast, e.span()))
+            .parse(
+                tokens
+                    .as_slice()
+                    .map((src.len()..src.len()).into(), |(t, s)| (t, s)),
+            )
+            .into_output_errors();
+
+        if let Some((funcs, file_span)) = ast.filter(|_| errs.len() + parse_errs.len() == 0) {
+            println!("Parsed: {funcs:?}");
+        }
+
+        parse_errs
+    } else {
+        Vec::new()
+    };
+
+    errs.into_iter()
+        .map(|e| e.map_token(|c| c.to_string()))
+        .chain(
+            parse_errs
+                .into_iter()
+                .map(|e| e.map_token(|tok| tok.to_string())),
+        )
+        .for_each(|e| {
+            Report::build(ReportKind::Error, (filename.clone(), e.span().into_range()))
+                .with_config(ariadne::Config::new().with_index_type(ariadne::IndexType::Byte))
+                .with_message(e.to_string())
+                .with_label(
+                    Label::new((filename.clone(), e.span().into_range()))
+                        .with_message(e.reason().to_string())
+                        .with_color(Color::Red),
+                )
+                .with_labels(e.contexts().map(|(label, span)| {
+                    Label::new((filename.clone(), span.into_range()))
+                        .with_message(format!("while parsing this {label}"))
+                        .with_color(Color::Yellow)
+                }))
+                .finish()
+                .print(sources([(filename.clone(), src.clone())]))
+                .unwrap()
+        });
 }
+
+// use syntax::minirust::*;
+//
+// fn main() {
+//     let filename = env::args().nth(1).expect("Expected file argument");
+//     let src = fs::read_to_string(&filename).expect("Failed to read file");
+//
+//     let (tokens, mut errs) = lexer().parse(src.as_str()).into_output_errors();
+//
+//     let parse_errs = if let Some(tokens) = &tokens {
+//         let (ast, parse_errs) = funcs_parser()
+//             .map_with(|ast, e| (ast, e.span()))
+//             .parse(
+//                 tokens
+//                     .as_slice()
+//                     .map((src.len()..src.len()).into(), |(t, s)| (t, s)),
+//             )
+//             .into_output_errors();
+//
+//         if let Some((funcs, file_span)) = ast.filter(|_| errs.len() + parse_errs.len() == 0) {
+//             if let Some(main) = funcs.get("main") {
+//                 if !main.args.is_empty() {
+//                     errs.push(Rich::custom(
+//                         main.span,
+//                         "The main function cannot have arguments".to_string(),
+//                     ))
+//                 } else {
+//                     match eval_expr(&main.body, &funcs, &mut Vec::new()) {
+//                         Ok(val) => println!("Return value: {val}"),
+//                         Err(e) => errs.push(Rich::custom(e.span, e.msg)),
+//                     }
+//                 }
+//             } else {
+//                 errs.push(Rich::custom(
+//                     file_span,
+//                     "Programs need a main function but none was found".to_string(),
+//                 ));
+//             }
+//         }
+//
+//         parse_errs
+//     } else {
+//         Vec::new()
+//     };
+//
+//     errs.into_iter()
+//         .map(|e| e.map_token(|c| c.to_string()))
+//         .chain(
+//             parse_errs
+//                 .into_iter()
+//                 .map(|e| e.map_token(|tok| tok.to_string())),
+//         )
+//         .for_each(|e| {
+//             Report::build(ReportKind::Error, (filename.clone(), e.span().into_range()))
+//                 .with_config(ariadne::Config::new().with_index_type(ariadne::IndexType::Byte))
+//                 .with_message(e.to_string())
+//                 .with_label(
+//                     Label::new((filename.clone(), e.span().into_range()))
+//                         .with_message(e.reason().to_string())
+//                         .with_color(Color::Red),
+//                 )
+//                 .with_labels(e.contexts().map(|(label, span)| {
+//                     Label::new((filename.clone(), span.into_range()))
+//                         .with_message(format!("while parsing this {label}"))
+//                         .with_color(Color::Yellow)
+//                 }))
+//                 .finish()
+//                 .print(sources([(filename.clone(), src.clone())]))
+//                 .unwrap()
+//         });
+// }
