0eda5e5bd9afea9551c017cad303b141c69d3b79
Niels Saurer <me@nielssaurer.com>
2025-07-23 22:13:02 +0200

    allow(unused)

--- grabapl/tests/util/semantics.rs
+++ grabapl/tests/util/semantics.rs
@@ -2,6 +2,9 @@ pub mod helpers;
 
 pub use grabapl::semantics::example::*;
 
+#[allow(unused)]
 pub use ExampleOperation as TestOperation;
+#[allow(unused)]
 pub use ExampleQuery as TestQuery;
+#[allow(unused)]
 pub use ExampleSemantics as TestSemantics;
