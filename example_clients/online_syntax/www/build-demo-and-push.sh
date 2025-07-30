7858d9de18c9cf77951fdfd69c0dbf812823f483
Niels Saurer <me@nielssaurer.com>
2025-07-30 23:46:29 +0200

    update webdemo build script

--- example_clients/online_syntax/www/build-demo-and-push.sh
+++ example_clients/online_syntax/www/build-demo-and-push.sh
@@ -4,6 +4,7 @@ set -e
 
 (cd .. && ./build.sh --release)
 rm dist/* && npm run build
+(cd ~/eth/msc-thesis/playground/grabapl-github-io && git checkout gh-pages && git pull)
 rm ~/eth/msc-thesis/playground/grabapl-github-io/public/playground/*
 cp dist/* ~/eth/msc-thesis/playground/grabapl-github-io/public/playground/
 (cd ~/eth/msc-thesis/playground/grabapl-github-io/public/playground && git add . && git commit -m "update playground" && git push)
