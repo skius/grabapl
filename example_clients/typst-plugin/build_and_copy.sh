7269d1c43fc65db841df15906863f5a0a129706c
Niels Saurer <me@nielssaurer.com>
2025-08-28 21:21:16 +0200

    typst-plugin build warning

--- example_clients/typst-plugin/build_and_copy.sh
+++ example_clients/typst-plugin/build_and_copy.sh
@@ -6,3 +6,5 @@ cp ../../target/wasm32-unknown-unknown/release/typst_plugin.wasm .
 wasm-opt typst_plugin.wasm -o typst_plugin_opt.wasm -O
 mv typst_plugin_opt.wasm typst_plugin.wasm
 mv typst_plugin.wasm typst-project/grabapl-package
+
+echo "Don't forget to copy the package to the msc-thesis/report directory"
